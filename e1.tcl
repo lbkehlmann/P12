e5158cedecaa4abd95d9735484668c60d55798a6
matthew <matthew@Tomix.(none)>
2014-12-11 23:12:07 -0600

    included tests

--- e1.tcl
+++ e1.tcl
@@ -18,6 +18,30 @@ expect "username> "
 send "ender\r"
 
 expect "ender> "
+send "ls\r"
+
+expect "ender> "
+send "cat test1.txt\r"
+
+expect "ender> "
+send "chuser\r"
+
+expect "username> "
+send "admin\r"
+
+expect "admin> "
+send "users.txt\r"
+
+expect "admin> "
+send "chuser\r"
+
+expect "username> "
+send "peter\r"
+
+expect "peter> "
+send "test0.txt\r"
+
+expect "peter> "
 send "shutdown\r"
 
 expect "*** System Shutdown ***\r"
